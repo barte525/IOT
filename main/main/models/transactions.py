d6cd339b318078d03441481b94ca745f61276fa2
barte525 <bartek5251@gmail.com>
2022-01-09 17:12:01 +0100

    zmiana pola

--- main/main/models/transactions.py
+++ main/main/models/transactions.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .card import Card
+from .tickets import Ticket
 import uuid
 
 
@@ -7,4 +8,4 @@ class Transaction(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     card = models.ForeignKey(Card, on_delete=models.CASCADE)
     date = models.DateField()
-    expiration_date = models.DateField()
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
