4c34624aec4147161868af2c369b79a0ec227af4
Patryk Stępień <p.stepien@poczta.pl>
2022-01-28 21:16:02 +0100

    Poprawa layoutu

--- main/main/urls.py
+++ main/main/urls.py
@@ -17,22 +17,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from main.views.manage_profiles import ManageProfiles, CardOwnerView, SellerView
-from main.views.main import Main
+from main.views.main import Main, OfferView
 from main.views.register import RegisterUser, RegisterUserResponse
 from main.views.seller_buy import SellerBuyTicket, SellerBuyTicketSave
-from main.views.card_owner import CardOwnerOfferView, CardOwnerStatusView, CardOwnerBuyTicketView, \
-    CardOwnerConfirmBuyTicketView
+from main.views.card_owner import CardOwnerStatusView, CardOwnerBuyTicketView, CardOwnerConfirmBuyTicketView
 from main.views.api import CardApi
 
 urlpatterns = [
     path('', Main.as_view()),
     path('admin/', admin.site.urls),
     path('zaloguj/', auth_views.LoginView.as_view(template_name="login.html")),
+    path('wyloguj/', auth_views.LogoutView.as_view(template_name="logout.html")),
+    path('oferta/', OfferView.as_view()),
     path('profil/', ManageProfiles.as_view()),
     path('profil/uzytkownikKarty/', CardOwnerView.as_view()),
     path('profil/sprzedawca/', SellerView.as_view()),
-    path('profil/zmianaHasla/', auth_views.PasswordChangeView.as_view(success_url='/profil/uzytkownikKarty/',
-                                                                          template_name='password_change.html')),
+    path('profil/zmianaHasla/', auth_views.PasswordChangeView.as_view(success_url='/profil/',
+                                                                      template_name='password_change.html')),
     path('profil/sprzedawca/', include([
         path('rejestracja/', RegisterUser.as_view()),
         path('rejestracja/', include([
@@ -51,7 +52,6 @@ urlpatterns = [
     # path('profil/sprzedawca/zakupBiletu/sukces', SellerBuyTicketSave.as_view()),
 
     path('profil/uzytkownikKarty/', include([
-        path('oferta', CardOwnerOfferView.as_view()),
         path('statusKarty', CardOwnerStatusView.as_view()),
         path('zakupBiletu/', include([
             path('<uuid:ticket_id>', CardOwnerBuyTicketView.as_view()),
--- main/main/views/main.py
+++ main/main/views/main.py
@@ -1,7 +1,15 @@
 from django.views import View
 from django.shortcuts import render
 
+from main.models import Ticket
+
 
 class Main(View):
     def get(self, request):
         return render(request, 'main_page.html')
+
+class OfferView(View):
+    def get(self, request):
+        tickets = Ticket.objects.all()
+
+        return render(request, 'offer.html', {'tickets': tickets})
